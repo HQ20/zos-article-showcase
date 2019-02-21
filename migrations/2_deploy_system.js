20bef140a5f34d64b629a6933d44251ed6b67050
Bernardo Vieira <bernardo5@live.com.pt>
2019-02-21 17:55:02 +0000

    update migrations for easy testing

--- migrations/2_deploy_system.js
+++ migrations/2_deploy_system.js
@@ -1,5 +1,10 @@
+var Roles = artifacts.require("./Roles.sol");
 var MyContract = artifacts.require("./MyContract.sol");
 
 module.exports = function (deployer) {
+    // deploy Roles
+    deployer.deploy(Roles);
+    deployer.link(Roles, MyContract);
+    // deploy MyContract
     deployer.deploy(MyContract);
 };

6f31fb20e912dd6e3c5bbb2bfc744cb7e9b3585c
Bernardo Vieira <bernardo5@live.com.pt>
2019-02-25 15:57:41 +0000

    add basic tests and update contracts deployment

--- migrations/2_deploy_system.js
+++ migrations/2_deploy_system.js
@@ -1,10 +1,14 @@
 var Roles = artifacts.require("./Roles.sol");
+var MockRoles = artifacts.require("./mock/MockRoles.sol");
 var MyContract = artifacts.require("./MyContract.sol");
 
-module.exports = function (deployer) {
+module.exports = function (deployer, network, accounts) {
     // deploy Roles
     deployer.deploy(Roles);
     deployer.link(Roles, MyContract);
+    deployer.link(Roles, MockRoles);
     // deploy MyContract
     deployer.deploy(MyContract);
+    // deploy MockRoles
+    deployer.deploy(MockRoles);
 };
--- test/MyContract.js
+++ test/MyContract.js
@@ -1,4 +1,5 @@
 const MyContract = artifacts.require('./MyContract.sol');
+const MockRoles = artifacts.require('./mock/MockRoles.sol');
 
 const BigNumber = require('bignumber.js');
 const chai = require('chai');
@@ -7,32 +8,47 @@ chai.use(require('chai-bignumber')()).should();
 
 contract('MyContract', (accounts) => {
     let myContract;
+    let mockRoles;
+    const mainAccount = accounts[0];
+    const zosDeployer = accounts[1];
+    const someUser = accounts[2];
+    const depositAmount = 10000000;
 
     before(async () => {
-        myContract = MyContract.deployed();
+        myContract = await MyContract.deployed();
+        mockRoles = await MockRoles.deployed();
     });
-
+    
     describe('Test some wallet methods', () => {
         before(async () => {
-            //
+            myContract = await MyContract.new();
+            myContract.initialize('Hello', { from: mainAccount });
         });
         it('add user wallet method', async () => {
-            //
+            await myContract.addUserWallet(someUser, { from: mainAccount });
         });
         it('deposit method', async () => {
-            //
+            await myContract.deposit({ value: depositAmount, from: someUser });
         });
         it('total user balance method', async () => {
-            //
+            const totalBalance = new BigNumber(await myContract.totalUsersBalance());
+            totalBalance.should.be.bignumber.equal(new BigNumber(depositAmount));
         });
     });
 
     describe('Test some role methods', () => {
         before(async () => {
-            //
+            myContract = await MyContract.new();
+            mockRoles = await MockRoles.new();
+            myContract.initialize('Hello', { from: mainAccount });
         });
         it('add role method', async () => {
-            //
+            // enum Role { None, Owner, Admin, Regist }
+            const roleId = 1;
+            assert.equal(await mockRoles.hasRole(someUser, roleId), false, '');
+            await mockRoles.setRole(someUser, roleId, { from: mainAccount });
+            assert.equal(await mockRoles.hasRole(someUser, roleId), true, '');
+            assert.equal(await mockRoles.hasRole(someUser, roleId + 1), true, '');
         });
     });
 });

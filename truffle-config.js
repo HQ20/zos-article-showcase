abf6b9fd5924706e6185279a573036564ade8b7b
Bernardo Vieira <bernardo5@live.com.pt>
2019-02-25 15:57:16 +0000

    renamed network in truffle-config

--- truffle-config.js
+++ truffle-config.js
@@ -2,7 +2,7 @@ module.exports = {
 
     // Define networks
     networks: {
-        development: {
+        local: {
             host: "127.0.0.1",
             port: 8545,
             network_id: "*",
